d13c9571415180ab9d32b2b016d5f35e15a25dba
johan sebastian calderon lamus <jhoan.calderon.313@gmail.com>
2024-10-07 23:05:40 -0500

    nuevo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -54,6 +54,9 @@ class AdminPanelProvider extends PanelProvider
                 DispatchServingFilamentEvent::class,
             ])
             //->plugin(FilamentSpatieRolesPermissionsPlugin::make())
+            ->plugin(
+                \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make()
+            )
             ->authMiddleware([
                 Authenticate::class,
             ])->favicon(asset('img/logo.png'));
