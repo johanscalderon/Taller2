9cb9e1518ae1137acad63b2393012915cb007396
johan sebastian calderon lamus <jhoan.calderon.313@gmail.com>
2024-10-08 11:02:46 -0500

    dd

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,20 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\Cliente;
+use App\Models\Compra;
+use App\Models\Producto;
+use App\Models\Proveedor;
+use App\Models\User;
+use App\Models\Venta;
+use App\Models\Role;
+use App\Policies\ClientePolicy;
+use App\Policies\CompraPolicy;
+use App\Policies\ProductoPolicy;
+use App\Policies\ProveedorPolicy;
+use App\Policies\RolePolicy;
+use App\Policies\UserPolicy;
+use App\Policies\VentaPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,7 +27,14 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        Cliente::class => ClientePolicy::class,
+        Producto::class => ProductoPolicy::class,
+        Compra::class => CompraPolicy::class,
+        Proveedor::class => ProveedorPolicy::class,
+        Role::class => RolePolicy::class,
+        User::class => UserPolicy::class,
+        Venta::class => VentaPolicy::class,
+
     ];
 
     /**
